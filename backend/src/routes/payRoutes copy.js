d558e547f1bff6054836b0130336e6a972ff54b6
brokercl <brokercl.@gmail.com>
2024-04-10 08:59:28 -0400

    boton de pago transbank ok

--- backend/src/routes/payRoutes copy.js
+++ backend/src/routes/payRoutes copy.js
@@ -26,37 +26,29 @@ router.post('/create-transaction', async (req, res) => {
         amount = req.body.amount,
         return_url = "http://www.comercio.cl/webpay/retorno"
       );
+  
 
         // Extract token and URL from the transaction response
         const { token, url } = transaction;
 
-        // Construir formulario HTML
+        /* Build HTML form 
+          using JavaScript to submit the form programmatically 
+          right after it's constructed
+          this way User avoid submit button
+          redirecting automatically to payment URL
+        */
         const form = `
-            <div style="text-align: center;">
-                <h2>¡Listo para pagar!</h2>
-                <p>Por favor, haz clic en el botón para proceder al pago.</p>
-                <form action="${url}" method="POST">
-                <p>URL de Pago: <strong>${url}</strong></p>
-                <p>Token de Transacción: <strong>${token}</strong></p>
-                    <input type="hidden" name="token_ws" value="${token}"/>
-                    <button type="submit" style="background-color: #4CAF50; /* Green */
-                                                border: none;
-                                                color: white;
-                                                padding: 15px 32px;
-                                                text-align: center;
-                                                text-decoration: none;
-                                                display: inline-block;
-                                                font-size: 16px;
-                                                margin: 4px 2px;
-                                                transition-duration: 0.4s;
-                                                cursor: pointer;
-                                                border-radius: 12px;">
-                        Pagar Ahora
-                    </button>
-                </form>
-            </div>
-        `;
-
+        <form id="paymentForm" action="${url}" method="POST">
+            <input type="hidden" name="token_ws" value="${token}"/>
+        </form>
+        <script>
+            // Automatically submit the form when the page loads
+            window.onload = function() {
+                document.getElementById('paymentForm').submit();
+            };
+        </script>
+    `;
+    
         // Enviar el formulario como respuesta
         res.send(form);
     } catch (error) {
